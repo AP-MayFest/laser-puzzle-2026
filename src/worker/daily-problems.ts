d0ef7725346ebabd9400ffc2808e205906101018
Yuki Ogawa <yuki_0gawa@icloud.com>
2026-05-02 17:42:50 +0900

    add problems

--- src/worker/daily-problems.ts
+++ src/worker/daily-problems.ts
@@ -8,17 +8,37 @@ const DAILY_PROBLEMS: readonly Problem[] = [
         date: '2026-05-01',
         problemCode: '5x5-4bRS5aGS2fSS4aRE1gNE2eNW1fSE1bGW8bRE1fPE1eNW7fPS7bGN-c0e1',
         difficulty: 'medium',
-        credit: {
-            author: 'ichi',
-        },
+        credit: { author: 'ichi' },
     },
     {
         date: '2026-05-02',
         problemCode: '4x4-1bGS5bRE4aGW15aRE4bGW5bRN-c6g2',
         difficulty: 'easy',
-        credit: {
-            author: 'hatena',
-        },
+        credit: { author: 'hatena' },
+    },
+    {
+        date: '2026-05-03',
+        problemCode: '5x5-2bGS7fPS2aGS4cSE4aRW3fPS7cNE4fSW1bRW2h1cNE1fSW1cNW-d2',
+        difficulty: 'easy',
+        credit: { author: 'miso' },
+    },
+    {
+        date: '2026-05-04',
+        problemCode: '7x7-46dNW1dNE4aGW3bGE4aRN4bRW13bRN2bGN-c0d0e1fP1fS1fD1g2',
+        difficulty: 'medium',
+        credit: { author: '2025年度工学博覧会光班' },
+    },
+    {
+        date: '2026-05-05',
+        problemCode: '3x3-5bRE3aGW8aRE3bGW-c6d1',
+        difficulty: 'easy',
+        credit: { author: 'hatena' },
+    },
+    {
+        date: '2026-05-06',
+        problemCode: '5x5-3bGS1bGS7fPS8aRW10aGE3fPE5fPS7bRN1bRN-d0g2',
+        difficulty: 'medium',
+        credit: { author: 'daifuku' },
     },
 ];
 
